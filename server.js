0ff772832fbd0c2376a3427a6d23de6b4ef74188
Pramod Gupta <P10484337@capita.co.uk>
2021-02-27 19:37:13 +0530

    before deployment

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin')
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const app = express();
+// app.use(express.json())
+// app.use(cors());
+
+const db = knex({
+    client: 'mssql',
+    connection: {
+        user: 'pramod',  
+        password: '',  
+        server: '127.0.0.1',  
+        database: 'smart-brain'
+    }
+  });
+
+// app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
+
+app.get('/', (req,res) => { res.json("success") })
+
+// app.post('/signin', (req,res) => { signin.handleSignin(req, res, db, bcrypt) })
+
+// app.put('/image', (req,res) => { image.handleImage(req, res, db)})
+// app.post('/imageurl', (req,res) => { image.handleClarifaiApi(req, res)})
+
+// app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
+
+app.listen(process.env.PORT || 4000,()=>{ 
+    console.log('listening at 4000');
+})
+
