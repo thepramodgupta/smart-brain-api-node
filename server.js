eee7350f465b5922a569c668d9bf47e8d738ff84
Pramod Gupta <P10484337@capita.co.uk>
2021-02-27 19:56:31 +0530

    commented end point

--- server.js
+++ server.js
@@ -22,14 +22,14 @@ const db = knex({
     }
   });
 
-app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) });
+// app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) });
 
-app.post('/signin', (req,res) => { signin.handleSignin(req, res, db, bcrypt) });
+// app.post('/signin', (req,res) => { signin.handleSignin(req, res, db, bcrypt) });
 
-app.put('/image', (req,res) => { image.handleImage(req, res, db)});
-app.post('/imageurl', (req,res) => { image.handleClarifaiApi(req, res)});
+// app.put('/image', (req,res) => { image.handleImage(req, res, db)});
+// app.post('/imageurl', (req,res) => { image.handleClarifaiApi(req, res)});
 
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
+// app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 
 app.get('/', (req, res) => {
     res.json('success');
